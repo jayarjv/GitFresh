3cc879fe8d53fe88c06841317cce9b10078535e6
jayarjv <jayarjv@gmail.com>
2015-10-13 10:42:56 +0100

    Change in Get value 2 3

--- MvcApplication1/MvcApplication1/Controllers/ValuesController.cs
+++ MvcApplication1/MvcApplication1/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace MvcApplication1.Controllers
 
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value2", "value3" };
         }
 
         // GET api/values/5
